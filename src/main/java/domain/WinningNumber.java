cedb85c5c346bb7d32f685feb52315907082228c
최창준 <aowert6518@gmail.com>
2022-01-25 11:02:00 +0900

    [feat] 당첨 번호를 생성한다

---
+++ src/main/java/domain/WinningNumber.java
@@ -0,0 +1,51 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class WinningNumber {
+    private List<Integer> numbers;
+
+    public WinningNumber(String input) {
+        isValidPattern(input);
+        numbers = convertStringToIntegerList(input);
+        isValidNumbers();
+        isValidDuplicate();
+    }
+
+    private void isValidPattern(String input) {
+        boolean matches = Pattern.matches("[0-9]+, [0-9]+, [0-9]+, [0-9]+, [0-9]+, [0-9]+", input);
+        if (!matches) {
+            throw new IllegalArgumentException("당첨번호를 잘 입력해주세요 (ex. 1, 10, 15, 32, 45, 42");
+        }
+    }
+
+    private void isValidNumbers() {
+        for (Integer number : numbers) {
+            isValidRange(number);
+        }
+    }
+
+    private void isValidRange(Integer number) {
+        if (number < 1 || number > 45) {
+            throw new IllegalArgumentException("1부터 45 사이의 수를 입력하세요");
+        }
+    }
+
+    private void isValidDuplicate() {
+        HashSet<Integer> hashSet = new HashSet<>(numbers);
+        if (hashSet.size() != 6) {
+            throw new IllegalArgumentException("중복된 숫자가 있습니다");
+        }
+    }
+
+    private List<Integer> convertStringToIntegerList(String input) {
+        return Arrays.stream(input.split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/domain/WinningNumberTest.java
@@ -0,0 +1,42 @@
+package domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WinningNumberTest {
+    @Test
+    public void correct_test() throws Exception{
+        Assertions.assertDoesNotThrow(
+                () -> new WinningNumber("1, 2, 3, 4, 5, 6")
+        );;
+    }
+
+    @Test
+    public void pattern_test() throws Exception {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new WinningNumber("1,2,3,4,5,6")
+        );
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new WinningNumber("a, b, c, d, e, f")
+        );
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new WinningNumber("")
+        );
+    }
+
+    @Test
+    public void duplicate_test() throws Exception{
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new WinningNumber("1, 1, 2, 3, 4, 5")
+        );
+    }
+
+    @Test
+    public void not_correct_test() throws Exception{
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new WinningNumber("0, 100, 222, 333, 444, 55")
+        );
+    }
+}
